c88bf9123dd1b9dd48045e81bde05cf28be9ffab
harsh56845 <56841891+harsh56845@users.noreply.github.com>
2026-09-05 22:18:19 +0530

    Time: 328 ms (77.42%), Space: 21.6 MB (67.74%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        n = len(nums)
+        postFix = [0]*n
+
+        for i in range(n-1,-1,-1):
+            if i<(n-1):
+                postFix[i] = min(nums[i],postFix[i+1])
+            else:
+                postFix[i]=nums[i]
+                
+        smallDiff = 9999999
+        ansIdx = -1
+        currMax = -99999999
+        for i in range(n):
+            currMax = max(currMax,nums[i])
+            diff = abs(currMax-postFix[i])
+            if diff<=k:
+                return i 
+                # and smallDiff>diff:
+                # smallDiff = min(smallDiff,diff)
+                # ansIdx = i
+
+        return ansIdx
